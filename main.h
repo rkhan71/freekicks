716d2fbc3542464f3c6d5a8658527feecece274d
rkhan71 <102491107+rkhan71@users.noreply.github.com>
2023-05-07 20:54:10 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "gba.h"
+
+// TODO: Create any necessary structs
+
+/*
+* For example, for a Snake game, one could be:
+*
+* struct snake {
+*   int heading;
+*   int length;
+*   int row;
+*   int col;
+* };
+*
+* Example of a struct to hold state machine data:
+*
+* struct state {
+*   int currentState;
+*   int nextState;
+* };
+*
+*/
+
+struct Positions {
+    int ballx;
+    int bally;
+    int wallx;
+    int wally;
+    int titlex;
+};
+
+#endif
